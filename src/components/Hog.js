4fc4f0823b03ba77aaacea676dcd22d32c0c73cc
GeorgeCoops <coopsgeorge@gmail.com>
2020-05-15 11:52:18 +0100

    Done.

--- src/components/Hog.js
+++ src/components/Hog.js
@@ -17,10 +17,15 @@ class Hog extends Component {
     render(){
         let pigImg = require(`../hog-imgs/${this.props.hog.name.toLowerCase().replace(" ","_").replace(" ","_")}.jpg`)
         return(
-            <div className="ui eight wide column">
-                <h2>{this.props.hog.name}</h2>
-                <img onClick={() => {this.props.showDetail(this.props.hog)} } src={pigImg} />
-                <button onClick={() => {this.props.renderWithoutHidden(this.props.hog)}}>Hide Pig</button> 
+            <div className="ui eight wide column" className="ui card" className="hog-card">
+                <div className="image" >
+                    <img onClick={() => {this.props.showDetail(this.props.hog)} } src={pigImg} />
+                </div>
+                <div className="content">
+                    <a className="header">{this.props.hog.name}</a>
+                    <button onClick={() => {this.props.renderWithoutHidden(this.props.hog)}}>Hide Pig</button> 
+                </div>
+
             </div>
         )
     }
